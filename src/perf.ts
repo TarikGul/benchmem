35ce05718e2fa2034fc54c29d7364f3f64b41254
tarikgul <tarik@parity.io>
2023-09-23 10:58:08 -0400

    add perf async support

--- src/perf.ts
+++ src/perf.ts
@@ -1,5 +1,17 @@
-export const perf = (fn: Function, ...args: any[]) => {
-    const start = performance.now();
-    fn(...args);
-    return performance.now() - start;
-}
+export const perf = (fn: Function, inpt: { iterations?: number }, ...args: unknown[]) => {
+	const start = performance.now();
+	const iter = inpt.iterations ? inpt.iterations : 1;
+	for (let i = 0; i < iter; i++) {
+		fn(...args);
+	}
+	return performance.now() - start;
+};
+
+export const perfAsync = async (fn: Function, inpt: { iterations?: number }, ...args: unknown[]) => {
+	const start = performance.now();
+	const iter = inpt.iterations ? inpt.iterations : 1;
+	for (let i = 0; i < iter; i++) {
+		await fn(...args);
+	}
+	return performance.now() - start;
+};
